c298a13ba99733481944d477cf6c1f31e08ee64d
Zachary Slade <106479794+stoating@users.noreply.github.com>
2022-10-25 23:36:12 +0200

    hiding the necessary env variables

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -8,7 +8,7 @@ import { z } from "zod";
 export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
-  NEXTAUTH_SECRET: z.string(),
+  //NEXTAUTH_SECRET: z.string(),
   NEXTAUTH_URL: z.preprocess(
     // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
     // Since NextAuth automatically uses the VERCEL_URL if present.
@@ -16,8 +16,8 @@ export const serverSchema = z.object({
     // VERCEL_URL doesnt include `https` so it cant be validated as a URL
     process.env.VERCEL ? z.string() : z.string().url(),
   ),
-  DISCORD_CLIENT_ID: z.string(),
-  DISCORD_CLIENT_SECRET: z.string(),
+  //DISCORD_CLIENT_ID: z.string(),
+  //DISCORD_CLIENT_SECRET: z.string(),
 });
 
 /**
