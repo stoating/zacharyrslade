2110981e69b593844a715a39863b1a9a9fc6a261
Zachary Slade <106479794+stoating@users.noreply.github.com>
2022-10-29 15:39:56 +0200

    honestly for this page, google alone is probably good

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook"; // Need privacy policy and data deletion
 
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
