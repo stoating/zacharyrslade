c9230767047eda2160b0430b4519ece31f64e490
Zachary Slade <106479794+stoating@users.noreply.github.com>
2022-10-31 15:27:49 +0100

    open external links in new tab

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,13 +8,13 @@ const Home: NextPage = () => {
   //const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
 
   const MenuItems = [
-    {id: 1, text: "About", ref: "/about"},
-    {id: 2, text: "Resume", ref: "/resume"},
-    {id: 3, text: "LinkedIn", ref: "https://www.linkedin.com/in/zacharyslade1"},
-    {id: 4, text: "Xing", ref: "https://www.xing.com/profile/Zachary_Slade"},
-    {id: 5, text: "GitHub", ref: "https://github.com/stoating"},
-    {id: 6, text: "Stoating", ref: "/stoating"},
-    {id: 7, text: "Contact", ref: "/contact"},
+    {id: 1, text: "About", target: "", ref: "/about"},
+    {id: 2, text: "Resume", target: "", ref: "/resume"},
+    {id: 3, text: "LinkedIn", target: "_blank", ref: "https://www.linkedin.com/in/zacharyslade1"},
+    {id: 4, text: "Xing", target: "_blank", ref: "https://www.xing.com/profile/Zachary_Slade"},
+    {id: 5, text: "GitHub", target: "_blank", ref: "https://github.com/stoating"},
+    {id: 6, text: "Stoating", target: "", ref: "/stoating"},
+    {id: 7, text: "Contact", target: "", ref: "/contact"},
   ]
 
   const handleMenuItemMouseOver = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
@@ -44,7 +44,7 @@ const Home: NextPage = () => {
         <div id="menu-items" className="mx-auto peer/menu-item hover:opacity-100 z-10 ring ring-black">
           {MenuItems.map(item => (
             <div key={item.id} id={"menu-item-div-"+item.id} className="inline-block group/item opacity-70 hover:opacity-100 ring ring-red-900">
-              <a id={"menu-item-"+item.id} className="text-white text-5xl font-sans inline-block opacity-80 group-hover/item:opacity-100 p-1 m-1 ring ring-green-900" href={item.ref} onMouseOver={handleMenuItemMouseOver}>{item.text}</a>
+              <a id={"menu-item-"+item.id} className="text-white text-5xl font-sans inline-block opacity-80 group-hover/item:opacity-100 p-1 m-1 ring ring-green-900" target={item.target} href={item.ref} onMouseOver={handleMenuItemMouseOver}>{item.text}</a>
             </div>
           ))}
         </div>
