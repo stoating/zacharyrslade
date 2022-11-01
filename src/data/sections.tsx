9e79087d0f6fd547bba666a13cd12c9f8838bfb7
Zachary Slade <106479794+stoating@users.noreply.github.com>
2022-11-01 17:18:33 +0100

    adding sections data for later

---
+++ src/data/sections.tsx
@@ -0,0 +1,9 @@
+export const Sections = {
+    Stoating: 'stoating',
+    Contact: 'contact',
+    About: 'about',
+    Resume: 'resume',
+    ProfessionalProfiles: 'professional-profiles',
+    Greeter: 'greeter',
+    Footer: 'footer',
+}
