41bc63fc41851e3833adf1db40cd0b6c04776286
Zachary Slade <106479794+stoating@users.noreply.github.com>
2022-11-03 22:56:14 +0100

    make vercel the happiest

--- src/components/navigation-bar.tsx
+++ src/components/navigation-bar.tsx
@@ -2,14 +2,13 @@ import { Socials } from "../data/socials";
 
 export const NavigationBar = () => {
   const NavigationBarItems = Socials.filter(function (Socials) {
-    return Socials.target !== "_blank";
+    return Socials.type !== "social";
   });
 
   return (
     <div className="fixed top-0 z-20 flex w-full bg-slate-500 ring ring-black">
       {NavigationBarItems.map((item) => (
         <div
-          key={item.id}
           className="mx-auto block bg-red-500 ring ring-red-600"
         >
           <a href={item.ref}>{item.text}</a>
