4c66581b6e8b6d823d9b6d6603947c7ecb42f410
Zachary Slade <106479794+stoating@users.noreply.github.com>
2022-11-13 09:08:42 +0100

    more link issues

--- src/components/navigation-bar.tsx
+++ src/components/navigation-bar.tsx
@@ -1,22 +1,24 @@
+import Link from "next/link";
+
 export const NavigationBar = () => {
   return (
     <div className="fixed top-0 z-20 flex w-full bg-slate-900 bg-opacity-50 text-opacity-100">
       <div className="mx-auto flex">
-        <a className="p-2 px-4" href="/#about">
-          About
-        </a>
-        <a className="p-2 px-4" href="/#resume">
-          Resume
-        </a>
-        <a className="p-2 px-4" href="/#profiles">
-          Profiles
-        </a>
-        <a className="p-2 px-4" href="/#stoating">
-          Stoating
-        </a>
-        <a className="p-2 px-4" href="/#contact">
-          Contact
-        </a>
+        <Link href="/#about">
+          <a className="p-2 px-4">About</a>
+        </Link>
+        <Link href="/#resume">
+          <a className="p-2 px-4">Resume</a>
+        </Link>
+        <Link href="/#profiles">
+          <a className="p-2 px-4">Profiles</a>
+        </Link>
+        <Link href="/#stoating">
+          <a className="p-2 px-4">Stoating</a>
+        </Link>
+        <Link href="/#contact">
+          <a className="p-2 px-4">Contact</a>
+        </Link>
       </div>
     </div>
   );
