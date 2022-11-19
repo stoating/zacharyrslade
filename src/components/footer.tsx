b240583dd98883f8957c3b9821195c8f67e8db8c
Zachary Slade <106479794+stoating@users.noreply.github.com>
2022-11-19 22:03:03 +0100

    footer formatting

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,21 +8,24 @@ export const Footer = () => {
 
   return (
     <footer className="bg-slate-700">
-      <div className="mx-auto flex max-w-screen-lg py-10 px-2">
+      <div className=" mx-auto flex max-w-screen-lg py-10 px-2">
         <div className="grid w-screen grid-cols-1 flex-col items-center md:grid-cols-6">
           <img
             src="/images/zack-look-down-bw-02.webp"
-            className="order-1 col-span-1 rounded-full opacity-60 hover:opacity-100 transition-all duration-300 ease-in-out shadow-2xl shadow-black"
+            className="order-1 col-span-1 rounded-full opacity-60 shadow-2xl shadow-black transition-all duration-300 ease-in-out hover:opacity-100"
           ></img>
           <div className="order-2 col-span-4">
             <img
               src="/images/signature-white-black-fade-01.webp"
-              className="opacity-70 hover:opacity-100 transition-all duration-300 ease-in-out"
+              className="opacity-70 transition-all duration-300 ease-in-out hover:opacity-100"
             ></img>
           </div>
-          <div className="order-3 col-span-1 h-40 rounded-full bg-aeb3bc hover:bg-white transition-all duration-300 ease-in-out shadow-2xl shadow-slate-900">
+          <div className="order-3 col-span-1 h-40 rounded-full bg-aeb3bc shadow-2xl shadow-slate-900 transition-all duration-300 ease-in-out hover:bg-white">
             <div className="pt-8 pl-10">
-              <div key="email" className="flex flex-col hover:text-rose-300 transition-all duration-300 ease-in-out">
+              <div
+                key="email"
+                className="flex flex-col transition-all duration-300 ease-in-out hover:text-rose-300"
+              >
                 <a className="flex" href="mailto:zack.slade@gmail.com">
                   <i className="pt-1 pr-2">
                     <MdEmail />
@@ -31,7 +34,10 @@ export const Footer = () => {
                 </a>
               </div>
               {ProfessionalLinks.map((item, index) => (
-                <div key={index} className="mx-auto hover:text-rose-300 transition-all duration-300 ease-in-out">
+                <div
+                  key={index}
+                  className="mx-auto transition-all duration-300 ease-in-out hover:text-rose-300"
+                >
                   <a
                     className="flex"
                     href={item.ref}
