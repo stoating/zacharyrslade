16523f7a3d7f116bfbd8c6af595455c15114eb56
Zachary Slade <106479794+stoating@users.noreply.github.com>
2022-11-20 12:24:53 +0100

    add curated experience for resume

--- src/data/greeter.tsx
+++ src/data/greeter.tsx
@@ -1,6 +1,6 @@
 export const greeterText = {
   snippet: {
-    en: "Lifelong learner, Chronically curious",
+    en: "Lifelong learner. Chronically curious",
     de: "Etwas Witziges, damit ich eingestellt werde",
   },
 };
--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -1,7 +1,21 @@
 export const resumeText = {
-  languages: [
-    {
-      english: {
+  header: {
+    name: "Zachary R. Slade",
+    address: "Langenburger Straße 51, 70435 Stuttgart",
+    phone: {
+      en: "Cell",
+      de: "Handy",
+      value: "+49 172 189 3085",
+    },
+    email: "zack.slade@gmail.com",
+  },
+  languages_section: {
+    key: "languages",
+    en: "Languages",
+    de: "Sprachen",
+    languages: [
+      {
+        key: "english",
         en: "English",
         de: "Englisch",
         rank: {
@@ -9,7 +23,8 @@ export const resumeText = {
           de: "Muttersprache",
         },
       },
-      german: {
+      {
+        key: "german",
         en: "German",
         de: "Deutsch",
         rank: {
@@ -17,8 +32,8 @@ export const resumeText = {
           de: "B1",
         },
       },
-    },
-  ],
+    ],
+  },
   skills_section: {
     key: "skills",
     en: "Skills",
@@ -125,15 +140,269 @@ export const resumeText = {
         ],
       },
     ],
-    header: {
-      name: "Zachary R. Slade",
-      address: "Langenburger Straße 51, 70435 Stuttgart",
-      phone: {
-        en: "Cell",
-        de: "Handy",
-        value: "+49 172 189 3085",
+  },
+  experiences_section: {
+    key: "experiences",
+    en: "Experience",
+    de: "Erfahrung",
+    companies: [
+      {
+        key: "company-1",
+        company: "Bosch Engineering GmbH",
+        positions: [
+          {
+            key: "position-1",
+            en: "Automation Engineer - Active Safety",
+            de: "Automatisierungsingeniuer - Active Safety",
+            location: {
+              en: "Stuttgart, Germany",
+              de: "Stuttgart, Deutschland",
+            },
+            dates: {
+              en: "May 2022 - Present",
+              de: "Mai 2022 - Heute",
+            },
+            bullets: [
+              {
+                key: "bullet-1",
+                en: "Complete project ownership from design to coding of a low-cost HIL for automated testing of active safety software",
+                de: "Vollständige Projektverantwortung eines kostenarm HIL für automatisierte Tests von active safety software",
+              },
+              {
+                key: "bullet-2",
+                en: "Coordinate with leadership to plan and prioritize DevOps activities with consideration for available resources and time",
+                de: "Planung und Priorisierung von DevOps-Aktivitäten unter Berücksichtigung der verfügbaren Ressourcen und Zeit",
+              },
+              {
+                key: "bullet-3",
+                en: "Manage students and coordinate with an international team to meet timing goals for various automation initiatives",
+                de: "Koordination mit Praktikanten und einem internationalen Team zur Einhaltung von Zeitzielen für Automatisierungsthemen",
+              },
+            ],
+          },
+          {
+            key: "position-2",
+            en: "Software Project Component Manager - Chassis Systems",
+            de: "Software Project Component Manager - Chassis Systems",
+            location: {
+              en: "Stuttgart, Germany",
+              de: "Stuttgart, Deutschland",
+            },
+            dates: {
+              en: "November 2017 - May 2022",
+              de: "November 2017 - Mai 2022",
+            },
+            bullets: [
+              {
+                key: "bullet-1",
+                en: "Software Project Component Manager for the largest, most profitable radar project and team in the Bosch Engineering Group",
+                de: "Software Project Component Manager für das größte und profitabelste Radarprojekt bei der Bosch Engineering GmbH",
+              },
+              {
+                key: "bullet-2",
+                en: "Primary customer contact for technical analysis and change requests to my projects code base",
+                de: "Hauptansprechpartner des Kunden für technische Analysen und Änderungswünsche an der Codebasis meines Projekts",
+              },
+              {
+                key: "bullet-3",
+                en: `Perform detailed project planning, requirements analysis, technical implementation, testing strategy, and numerous
+                supporting tasks across multiple parts of the code base for an international, diverse team of engineers`,
+                de: `Durchführung von detaillierter Projektplanung, Anforderungsanalyse, technischer Implementierung, Teststrategie und
+                diversen unterstützenden Aufgaben in mehreren Teilen der Codebasis für ein internationales, vielfältiges Ingenieurteam`,
+              },
+            ],
+          },
+        ],
       },
-      email: "zack.slade@gmail.com",
-    },
+      {
+        key: "company-2",
+        company: "White Tie Games LLC",
+        positions: [
+          {
+            key: "position-1",
+            en: "Co-Founder",
+            de: "Mitgründer",
+            location: {
+              en: "Akron, OH, USA",
+              de: "Akron, OH, Vereinigte Staaten",
+            },
+            dates: {
+              en: "August 2015 - July 2017",
+              de: "August 2015 - Juli 2017",
+            },
+            bullets: [
+              {
+                key: "bullet-1",
+                en: "Design, build and manage an online marketplace selling PC videogames using an affiliate program with online personalities",
+                de: `Design, Aufbau und Verwaltung eines Online-Marketplace für PC-Videospiele unter Verwendung eines Partnerprogramms mit
+                Online-Personalities`,
+              },
+              {
+                key: "bullet-2",
+                en: "Full-Stack web development using the Laravel MVC Framework incl. SQL database design, PHP, HTML, CSS, and Javascript",
+                de: "Full-Stack-Webentwicklung mit dem Laravel MVC Framework inkl. SQL-Datenbankdesign, PHP, HTML, CSS und Javascript",
+              },
+              {
+                key: "bullet-3",
+                en: "Developed website design skills, UX, art asset development, IT, and Amazon Web Services (AWS) including EC2 servers and S3",
+                de: "Entwickelte Fähigkeiten in den Bereichen Website-Design, UX, Entwicklung von Grafiken, IT und Amazon Web Services (AWS)",
+              },
+              {
+                key: "bullet-4",
+                en: "Account management and team management for building art assets and managing affiliate accounts",
+                de: "Account-Management und Team-Management für den Aufbau von Grafiken und die Verwaltung von Partner-Accounts",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        key: "company-3",
+        company: "Bosch Engineering Group",
+        positions: [
+          {
+            key: "position-1",
+            en: "Engineering Chassis Software Engineer",
+            de: "Engineering Chassis Softwareingenieur",
+            location: {
+              en: "Farmington Hills, MI, USA",
+              de: "Farmington Hills, MI, Vereinigte Staaten",
+            },
+            dates: {
+              en: "January 2014 - January 2017",
+              de: "Januar 2014 - Januar 2017",
+            },
+            bullets: [
+              {
+                key: "bullet-1",
+                en: "Performed my role as Complete Software Project Responsible and oversaw the timely release of multiple projects yearly",
+                de: "Gesamt-Softwareprojekt Verantwortlich. Überwachte die rechtzeitige Freigabe von mehreren Projekten pro Jahr",
+              },
+              {
+                key: "bullet-2",
+                en: "Led small teams and coordinated with other software groups to ensure successful project releases",
+                de: "Leitete kleine Teams und koordinierte mit anderen Softwaregruppen, um erfolgreiche Projektfreigaben zu gewährleisten",
+              },
+              {
+                key: "bullet-3",
+                en: "Acted as Diagnostic Communication and Diagnostic Software Responsible for all departmental programs",
+                de: "Verantwortlich für die Diagnosekommunikation und die Diagnosesoftware für alle Programme der Abteilung",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        key: "company-4",
+        company: "Robert Bosch LLC",
+        positions: [
+          {
+            key: "position-1",
+            en: "Complete Software Project Responsible",
+            de: "Gesamt-Softwareprojekt Verantwortlich",
+            location: {
+              en: "Farmington Hills, MI, USA",
+              de: "Farmington Hills, MI, Vereinigte Staaten",
+            },
+            dates: {
+              en: "February 2012 - August 2012",
+              de: "Februar 2012 - August 2012",
+            },
+            bullets: [
+              {
+                key: "bullet-1",
+                en: "Successfully developed customer logic, network communication, diagnostics, and software integrations for electronic steering control systems",
+                de: "Entwicklung von Kundenlogik, Net-Kommunikation, Diagnose und Software-Integrationen für elektronische Lenkungssysteme",
+              },
+              {
+                key: "bullet-2",
+                en: "Produced safety critical automotive software in a large group under strict time constraints",
+                de: "Erstellung von sicherheitskritischer Software für Kraftfahrzeuge in einer großen Gruppe unter strengen Zeitvorgaben",
+              },
+            ],
+          },
+          {
+            key: "position-2",
+            en: "Manufacturing Execution Systems (MES) Engineer",
+            de: "Manufacturing Execution Systems (MES) Ingenieur",
+            location: {
+              en: "Anderson, SC, USA",
+              de: "Anderson, SC, Vereinigte Staaten",
+            },
+            dates: {
+              en: "August 2012 - February 2013",
+              de: "August 2012 - Februar 2013",
+            },
+            bullets: [
+              {
+                key: "bullet-1",
+                en: "Built SQL databases, data acquisition programs and HTML webpages for the Planar Sensor Element manufacturing line",
+                de: "Erstellung von SQL-Datenbanken, Datenerfassungsprogrammen und HTML-Webseiten für das Planar Sensor Element (PSE) Fertigungslinie",
+              },
+              {
+                key: "bullet-2",
+                en: "Reduced failed move ticket transaction time for the PSE by 39%",
+                de: "Reduzierung der Transaktionszeit für fehlgeschlagene Transfers bei der PSE um 39%",
+              },
+            ],
+          },
+          {
+            key: "position-3",
+            en: "Automotive Radar Systems Sofware Engineer",
+            de: "Automotive Radar Systems Softwareingenieur",
+            location: {
+              en: "Abstatt, Germany",
+              de: "Abstatt, Deutschland",
+            },
+            dates: {
+              en: "February 2013 - August 2013",
+              de: "Februar 2013 - August 2013",
+            },
+            bullets: [
+              {
+                key: "bullet-1",
+                en: "Developed radar software for the Porsche Cajun, Porsche E2 II, and VW PQx",
+                de: "Entwicklung von Radarsoftware für den Porsche Cajun, Porsche E2 II und VW PQx",
+              },
+              {
+                key: "bullet-2",
+                en: "Dramatically reduced endurance test sorting procedure from an overnight process to under 2 hours",
+                de: "Dramatische Reduzierung des Sortierverfahrens für Dauertests von einer Nacht auf weniger als 2 Stunden",
+              },
+            ],
+          },
+          {
+            key: "position-4",
+            en: "Automotive Engineering Sales Associate",
+            de: "Automotive Engineering Vertriebsmitarbeiter",
+            location: {
+              en: "Farmington Hills, MI, USA",
+              de: "Farmington Hills, MI, Vereinigte Staaten",
+            },
+            dates: {
+              en: "August 2012 - February 2013",
+              de: "August 2012 - Februar 2013",
+            },
+            bullets: [
+              {
+                key: "bullet-1",
+                en: "Created websites, advertisements, and images for Bosch central and head up displays",
+                de: "Erstellung von Websites, Werbungen und Bildern für Bosch Central und Head-up-Displays",
+              },
+              {
+                key: "bullet-2",
+                en: "Accurately predicted Model Year 2014 Silverado base trim radio take rate within 0.5%",
+                de: "Genaue Vorhersage der Abnahmerate der Silverado-Basisausstattung für das Modelljahr 2014 mit einer Genauigkeit von 0,5",
+              },
+              {
+                key: "bullet-3",
+                en: "Led a project to create a fully traceable, streamlined flash and deliver process for development central displays",
+                de: "Leitung eines Projekts zur Schaffung eines vollständig rückverfolgbaren, Flash und Lieferprozesses für Central Displays",
+              },
+            ],
+          },
+        ],
+      },
+    ],
   },
 };
