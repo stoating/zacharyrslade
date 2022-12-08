ecb64376c6d65ad92af359974d5006171e95110d
Zachary Slade <106479794+stoating@users.noreply.github.com>
2022-12-08 22:00:44 +0100

    add german to contact

---
+++ src/data/contact.tsx
@@ -0,0 +1,22 @@
+export const contactText = {
+    title: {
+      en: "Reach Out",
+      de: "Kontaktiere mich",
+    },
+    name: {
+        en: "Name",
+        de: "Name",
+    },
+    email: {
+        en: "Email",
+        de: "Email",
+    },
+    message: {
+        en: "Message",
+        de: "Nachricht",
+    },
+    submit: {
+        en: "Send Message",
+        de: "Nachricht senden",
+    },
+  };
