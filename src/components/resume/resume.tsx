b0ac116903ef84f9501e7789eb74705d87acf2bd
stoating <zack.slade@gmail.com>
2024-10-22 21:25:33 +0200

    organize imports resume

--- src/components/resume/resume.tsx
+++ src/components/resume/resume.tsx
@@ -1,6 +1,6 @@
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 
 import { ContextLanguage } from "../../context/language";
 import { Language } from "../../data/_types";
