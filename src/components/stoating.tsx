5b512332e4d162dc90814875d8e907d297851d99
stoating <zack.slade@gmail.com>
2024-10-27 23:58:01 +0100

    make more relative

--- src/components/stoating.tsx
+++ src/components/stoating.tsx
@@ -8,13 +8,15 @@ export const Stoating = () => {
     id: string;
     x: number;
     y: number;
+    ratio: number;
+    factor: number;
     vx: number;
     vy: number;
   }
 
   type Axis = {
-    x: 'x';
-    y: 'y';
+    x: "x";
+    y: "y";
   };
 
   useEffect(() => {
@@ -29,16 +31,16 @@ export const Stoating = () => {
     init();
 
     function init() {
-      createStoat("badger");
-      createStoat("ferret");
-      createStoat("stoat-top-hat");
-      createStoat("stoat");
-      createStoat("weasel");
+      createStoat("badger", 920/400, 100);
+      createStoat("ferret", 920/400, 100);
+      createStoat("stoat-top-hat", 640/350, 110);
+      createStoat("stoat", 350/350, 150);
+      createStoat("weasel", 920/400, 100);
 
       animate();
     }
 
-    function createStoat(id: string) {
+    function createStoat(id: string, ratio: number, factor: number) {
       const dirX = Math.random() < 0.5 ? -1 : 1;
       const dirY = Math.random() < 0.5 ? -1 : 1;
 
@@ -46,6 +48,8 @@ export const Stoating = () => {
         id: id,
         x: Math.random() * boundaryX,
         y: Math.random() * boundaryY,
+        ratio: ratio,
+        factor: factor,
         vx: dirX * (Math.random() * vFactor + vOffset),
         vy: dirY * (Math.random() * vFactor + vOffset),
       };
@@ -54,21 +58,31 @@ export const Stoating = () => {
     }
 
     function changeDirection(stoat: Stoat, axis: keyof Axis) {
-      if (axis == 'x') {
+      if (axis == "x") {
         stoat.vx *= -1;
       } else {
         stoat.vy *= -1;
       }
     }
 
-    function drawStoat(id: string, x: number, y: number) {
+    function drawStoat(
+      id: string,
+      x: number,
+      y: number,
+      ratio: number,
+      factor: number
+    ) {
       const canvas = canvasRef.current;
       if (!canvas) return;
+
       const context = canvas.getContext("2d");
       const image = document.getElementById(id) as HTMLImageElement;
 
+      const width = ratio * factor;
+      const height = ratio * factor;
+
       if (context && image) {
-        context.drawImage(image, x, y, 180, 100);
+        context.drawImage(image, x, y, width, height);
       }
     }
 
@@ -76,7 +90,7 @@ export const Stoating = () => {
       stoats.forEach((stoat) => {
         stoat.x += stoat.vx;
         stoat.y += stoat.vy;
-        drawStoat(stoat.id, stoat.x, stoat.y);
+        drawStoat(stoat.id, stoat.x, stoat.y, stoat.ratio, stoat.factor);
 
         if (stoat.x < 0 || stoat.x > boundaryX) {
           changeDirection(stoat, "x");
@@ -88,9 +102,11 @@ export const Stoating = () => {
 
     function animate() {
       const canvas = canvasRef.current;
-      if (!canvas) return; // Add null check
+      if (!canvas) return;
+
       const context = canvas.getContext("2d");
       if (!context) return;
+
       context.clearRect(0, 0, 1000, 1000);
       draw();
       requestAnimationFrame(animate);
@@ -114,16 +130,16 @@ export const Stoating = () => {
         ></canvas>
         <div className="invisible">
           <Image
-            src="/images/stoating-weasel.png"
-            id="weasel"
-            alt="weasel"
+            src="/images/stoating-badger.png"
+            id="badger"
+            alt="badger"
             width={10000}
             height={10000}
           />
           <Image
-            src="/images/stoating-badger.png"
-            id="badger"
-            alt="badger"
+            src="/images/stoating-ferret.png"
+            id="ferret"
+            alt="ferret"
             width={10000}
             height={10000}
           />
@@ -142,9 +158,9 @@ export const Stoating = () => {
             height={10000}
           />
           <Image
-            src="/images/stoating-ferret.png"
-            id="ferret"
-            alt="ferret"
+            src="/images/stoating-weasel.png"
+            id="weasel"
+            alt="weasel"
             width={10000}
             height={10000}
           />
